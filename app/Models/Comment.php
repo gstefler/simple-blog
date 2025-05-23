43b229c2f7dd5255a29f443ba9e4e541d38b5ab9
Gabor Stefler <gabor.stefler@gmail.com>
2025-05-23 19:09:57 +0200

    created models, migrations and factories

---
+++ app/Models/Comment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'content',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
